442d53b5393da6e7e34f8d22431cf29403697fce
Dastan Ozgeldi <ozgdastan@gmail.com>
2021-08-17 14:18:54 +0600

    ✅ Remove useless pieces of code

--- passgen/__init__.py
+++ passgen/__init__.py
@@ -1,3 +1,8 @@
+"""
+Passgen
+=======
 
-__version__ = 'final 1.0'
-__author__ = 'Dastan Ozgeldi'
+A simple-as-hell password generator CLI.
+"""
+
+__version__ = '1.0.0'
--- passgen/__main__.py
+++ passgen/__main__.py
@@ -1,21 +1,13 @@
-from .cli import parse_flags
-from .exceptions import LengthNotInRange
-from .utils import generate_password
+from passgen.cli import parse_flags
+from passgen.utils import generate_password
 
 
 def main():
-    """The main function of the password generator.
-
-    Raises:
-        LengthNotInRange: When the given length did not follow limits.
-    """
+    """The main function of passgen."""
     args = parse_flags()
     length = int(input('Enter whatever length you want: '))
 
-    if not 4 <= length <= 50:
-        raise LengthNotInRange(length)
-
-    print(generate_password(length, args=args))
+    print(generate_password(length, args))
 
 
 if __name__ == '__main__':
